c2b3c7ef6c76619ac545674cf2d6790e45d8dc0f
Felix <zellerf24@barringtonschools.org>
2022-03-19 15:20:53 -0400

    Ajax task list with all tasklist memory model features

--- app/controllers/TaskList2.scala
+++ app/controllers/TaskList2.scala
@@ -8,7 +8,41 @@ import models.TaskListInMemoryModel
 
 @Singleton
 class TaskList2 @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
-  def load = Action {
-    Ok("Single Page")
+  def load: Action[AnyContent] = Action { implicit request =>
+    Ok(views.html.TaskList2())
+  }
+  def login = Action {
+    Ok(views.html.Login2())
+  }
+  def validate(username: String, password: String) = Action {
+    if (TaskListInMemoryModel.validateUser(username, password)) {
+      Ok(views.html.TaskList2TaskView(TaskListInMemoryModel.getTasks(username))).withSession("username" -> username)
+    }
+    else {
+      Ok(views.html.Login2())
+    }
+  }
+  def create(username: String, password: String) = Action {
+    if (TaskListInMemoryModel.createUser(username, password)) {
+      Ok(views.html.TaskList2TaskView(TaskListInMemoryModel.getTasks(username))).withSession("username" -> username)
+    }
+    else {
+      Ok(views.html.Login2())
+    }
+  }
+  def delete(index: Int): Action[AnyContent] = Action { implicit request =>
+    request.session.get("username").map { username =>
+      TaskListInMemoryModel.removeTask(username, index)
+      Ok(views.html.TaskList2TaskView(TaskListInMemoryModel.getTasks(username)))
+    }.getOrElse(Ok(views.html.Login2()))
+  }
+  def addTask(task: String) = Action { implicit request =>
+    request.session.get("username").map { username =>
+      TaskListInMemoryModel.addTask(username, task)
+      Ok(views.html.TaskList2TaskView(TaskListInMemoryModel.getTasks(username)))
+    }.getOrElse(Ok(views.html.Login2()))
+  }
+  def logout = Action {
+    Redirect(routes.TaskList2.load).withNewSession
   }
 }
---
+++ public/javascripts/taskListV2.js
@@ -0,0 +1,21 @@
+$("#contents").load("/login2")
+
+function login(){
+    const username = $("#loginName").val()
+    const password = $("#loginPass").val()
+    $("#contents").load("/validate2?username=" + username + "&password=" + password)
+}
+function createUser(){
+    const username = $("#createName").val()
+    const password = $("#createPass").val()
+    $("#contents").load("/create2?username=" + username + "&password=" + password)
+}
+
+function deleteTask(index){
+    $("#contents").load("/deleteTask2?index=" + index)
+}
+
+function addTask(){
+    const taskName = $("#newTask").val()
+    $("#contents").load("addTask2?task=" + encodeURIComponent(taskName))
+}
