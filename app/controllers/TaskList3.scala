4d91da47a007b02a7bf611cb11091a1c36e07ecb
Felix <zellerf24@barringtonschools.org>
2022-04-08 21:37:10 -0400

    First JSON method
    Will continue to work on this

--- app/controllers/TaskList3.scala
+++ app/controllers/TaskList3.scala
@@ -1,4 +1,17 @@
 package controllers
+import javax.inject._
 
-class TaskList3 {
+import play.api.mvc._
+import play.api.i18n._
+import models.TaskListInMemoryModel
+import play.api.libs.json.Json
+
+@Singleton
+class TaskList3 @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+  def load: Action[AnyContent] = TODO
+
+  def data = Action {
+    Ok(Json.toJson(Seq("a", "b", "c")))
+  }
 }
+
